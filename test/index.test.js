7e6ac365e76f861e99ccfa8aec8725318e236514
Dan Hogan <hogan.daniel@hotmail.com>
2019-07-08 14:12:44 -0700

    Add first test case

--- test/index.test.js
+++ test/index.test.js
@@ -1,1 +1,10 @@
-describe('returns words that do not contain the letter y')
+const removeYWords = require('../lib/index');
+
+describe('returns words from array that dont contain the letter y', () => {
+  it('returns correct array', () => {
+    const arr = ['sky', 'hot', 'Yeti', 'green'];
+    const expected = ['Is it hot?', 'Is it green?'];
+    const result = removeYWords(arr);
+    expect(result).toEqual(expected);
+  });
+});
